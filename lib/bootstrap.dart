a2f9b29a45f5c6289cb87b7d6bb7325b7bfde105
LuckUVeryX <luckuveryx@icloud.com>
2024-05-14 00:24:23 +0800

    fix: formatting

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -3,7 +3,6 @@ import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
 
-
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
